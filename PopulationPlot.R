ad2c3a61765cb313a4ea3d0da35ee965744f5e0a
ABHISHEK CHOUDHARY <abhishek.create@gmail.com>
2014-12-01 17:33:50 +0000

    Image uploaded

--- PopulationPlot.R
+++ PopulationPlot.R
@@ -89,7 +89,7 @@ plot(sites)
 ## Example with address, here UK post-code and some html code in tooltip
 
 df <- data.frame(Postcode=c("John Henchy & Sons,40 Wellington Road, St Lukes,Cork,Ireland",
-                            "Brú Bar & Hostel,57 MacCurtain St,Cork,Ireland",
+                            "Bru Bar & Hostel,57 MacCurtain St,Cork,Ireland",
                             "Windsor Inn Cork,54 - 55 MacCurtain St,Cork,Ireland",
                             "Dan Lowry's Tavern,13 MacCurtain St,Cork,Ireland",
                             "Shelbourne Bar , 16/17 MacCurtain Street Cork,Ireland",
@@ -102,25 +102,25 @@ df <- data.frame(Postcode=c("John Henchy & Sons,40 Wellington Road, St Lukes,Cor
                             "The Bowery,21 Tuckey St,Cork,Ireland"
                             ),
                  Tip=c("<a>John Henchy & Sons <b>(4:00-4:30)</b></a>",
-                       "Bru Bar(4:35-5:05)",
-                       "LV(5:10-5:40)",
-                       "Dan Lowry's Tavern(5:45-6:15)",
-                       "Shelbourne Bar(6:20-6:50)",
-                       "The Cork Arms(6:55-7:25)",
-                       "Gallagher's Pub(7:30-8:00)",
-                       "6ix(8:05-8:35)",
-                       "The Corner House(9:10-9:40)",
-                       "Sin E(9:45-10:15)",
-                       "Cashman(10:20-10:50)s",
-                       "The Slate(11:00-till death...)"))
+                       "<a>Bru Bar <b>(4:35-5:05)</b></a>",
+                       "<a>LV <b>(5:10-5:40)</b></a>",
+                       "<a>Dan Lowry's Tavern <b>(5:45-6:15)</b></a>",
+                       "<a>Shelbourne Bar <b>(6:20-6:50)</b></a>",
+                       "<a>The Cork Arms <b>(6:55-7:25)</b></a>",
+                       "<a>Gallagher's Pub <b>(7:30-8:00)</b></a>",
+                       "<a>6ix <b>(8:05-8:35)</b></a>",
+                       "<a>The Corner House <b>(9:10-9:40)</b></a>",
+                       "<a>Sin E <b>(9:45-10:15)</b></a>",
+                       "<a>Cashmans <b>(10:20-10:50)</b></a>",
+                       "<a>The Slate <b>(11:00-till death...)</b></a>"))
 
 M2 <- gvisMap(df, "Postcode", "Tip",
               options=list(showTip=TRUE,showLine=TRUE, mapType='normal',
                            enableScrollWheel=TRUE,
                            useMapTypeControl=TRUE,
                            icons=paste0("{",
-                                        "'default': {'normal': 'https://d30y9cdsu7xlg0.cloudfront.net/png/14871-42.png',\n",
-                                        "'selected': 'https://d30y9cdsu7xlg0.cloudfront.net/png/9454-42.png'",
+                                        "'default': {'normal': 'http://res.cloudinary.com/dgxal1qkf/image/upload/v1417455127/1_u5z1re.png',\n",
+                                        "'selected': 'http://res.cloudinary.com/dgxal1qkf/image/upload/v1417455196/2_ebvcx0.png'",
                                         "}}")))
 
 plot(M2)
